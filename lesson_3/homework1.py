eb6e6ab10ed41dbfd4060573826d4c82a22d7ab2
timohak777 <timohak777@gmail.com>
2023-12-04 20:18:15 +0200

    Finish Task 3 & 4

---
+++ lesson_3/homework1.py
@@ -0,0 +1,23 @@
+number1=int(input("Введіть перше число: "))
+number2=int(input("Введіть друге число: "))
+
+# Перший варіант
+if number1>number2:
+    print(f"Більше з двох чисел це {number1}")  
+
+if number1<number2:
+    print(f"Більше з двох чисел це {number2}")  
+
+if number1==number2:
+    print(f"Числа рівні {number1}")  
+
+# Другий варіант 
+
+# if number1-number2>0:
+#     print(f"Більше з двох чисел це {number1}")  
+
+# if number1-number2<0:
+#     print(f"Більше з двох чисел це {number2}")  
+
+# if number1-number2==0:
+#     print(f"Числа рівні {number1}")  
---
+++ lesson_3/homework2.py
@@ -0,0 +1,4 @@
+number1=int(input("Введіть ціну за кг: "))
+number2=int(input("Введіть скільки ви хочете кг: "))
+print(f"Добуток:{number1*number2}")
+
