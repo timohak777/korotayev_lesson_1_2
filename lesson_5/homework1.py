8bcc2298897cad6faea8aa30a7acee96a4332597
timohak777 <timohak777@gmail.com>
2023-12-18 19:10:16 +0200

    add lesson_5 task#1&2

---
+++ lesson_5/homework1.py
@@ -0,0 +1,6 @@
+wallet={
+    "Owner's name": "Timofei",
+    "Amount of money": 45678
+}
+N=wallet["Amount of money"]/37
+print(f"На вашому рахунку:{N}доларів. ")
---
+++ lesson_5/homework2.py
@@ -0,0 +1,14 @@
+numbers={
+    "number1":346,
+    "number2":560,
+}
+if numbers["number1"]>numbers["number2"] :
+    print("Number1 більше")
+
+if numbers["number1"]<numbers["number2"] :
+    print("Number2 більше")    
+
+
+
+#Створити словник, який описує два числа і потім за допомогою команди IF вивести на екран більше число 
+    
